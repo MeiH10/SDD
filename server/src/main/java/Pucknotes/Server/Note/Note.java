566945858186ec982d59d9052d84ffa0a0e94c04
mvhutz <pannus@maximhutz.me>
2024-11-05 07:20:24 -0500

    fix: lombok note

--- server/src/main/java/Pucknotes/Server/Note/Note.java
+++ server/src/main/java/Pucknotes/Server/Note/Note.java
@@ -2,55 +2,31 @@ package Pucknotes.Server.Note;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
 import org.bson.types.Binary;
 
+@AllArgsConstructor
+@RequiredArgsConstructor
+@NoArgsConstructor
 @Document(collection = "notes")
 public class Note {
 
     @Id
+    @Getter @Setter
     private String id;
+
+    @Getter @Setter
+    @NonNull
     private String title;
+
+    @Getter @Setter
+    @NonNull
     private Binary image;
-
-    // No-argument constructor
-    public Note() {
-    }
-
-    // Constructor with all fields
-    public Note(String id, String title, Binary image) {
-        this.id = id;
-        this.title = title;
-        this.image = image;
-    }
-
-    // Constructor with title and image only (without id)
-    public Note(String title, Binary image) {
-        this.title = title;
-        this.image = image;
-    }
-
-    // Getters and Setters
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Binary getImage() {
-        return image;
-    }
-
-    public void setImage(Binary image) {
-        this.image = image;
-    }
 }
