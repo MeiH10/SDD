ad71d6d244264ae3412374fb6cad37b5eb55e4bf
David Fong <dfong633@gmail.com>
2024-11-17 14:35:18 -0500

    changed fixed

--- server/src/main/java/Pucknotes/Server/session/SessionService.java
+++ server/src/main/java/Pucknotes/Server/session/SessionService.java
@@ -2,11 +2,13 @@ package Pucknotes.Server.Session;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import Pucknotes.Server.Account.Account;
 import Pucknotes.Server.Account.AccountService;
 import jakarta.servlet.http.HttpServletRequest;
 
+@Service
 public class SessionService {
     @Autowired
     private AccountService accountService;
