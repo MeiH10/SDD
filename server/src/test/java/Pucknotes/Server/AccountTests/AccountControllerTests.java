7432d43cfe34c1879e5745a22200f1813fd799eb
Veevek Dave <veevekd401@gmail.com>
2024-11-22 18:11:58 -0500

    set up registerAccount() tests

--- server/src/test/java/Pucknotes/Server/AccountTests/AccountControllerTests.java
+++ server/src/test/java/Pucknotes/Server/AccountTests/AccountControllerTests.java
@@ -40,4 +40,23 @@ class AccountControllerTest {
         accountController.verify_service = verificationService;
         accountController.sessionService = sessionService;
     }
+
+    @Test
+    void testCreateAccount() {
+        String token = "validToken";
+        String registration = "validRegistration";
+        Account mockAccount = new Account("test@example.com", "testUser", "password");
+        mockAccount.setId("accountId");
+
+        when(verificationService.verifyToken(registration, token)).thenReturn(mockAccount);
+        when(accountService.registerAccount(mockAccount)).thenReturn(mockAccount);
+
+        ResponseEntity<APIResponse<String>> response = accountController.createAccount(token, registration);
+
+        assertNotNull(response);
+        assertEquals("accountId", response.getBody().getData());
+        verify(verificationService, times(1)).verifyToken(registration, token);
+        verify(accountService, times(1)).registerAccount(mockAccount);
+    }
+    
 }
