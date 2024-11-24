347aa6430cc8f482c1d90f2d9f93757ebd36b9e0
Veevek Dave <veevekd401@gmail.com>
2024-11-24 15:27:47 -0500

    bug fix

--- server/src/main/java/Pucknotes/Server/Report/Report.java
+++ server/src/main/java/Pucknotes/Server/Report/Report.java
@@ -1,4 +1,10 @@
-import org.bson.types.ObjectId;
+package Pucknotes.Server.Report;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.FieldType;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
