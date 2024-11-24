6798e9a80f0e389618091c1857e300f1ea10807d
Veevek Dave <veevekd401@gmail.com>
2024-11-24 15:32:45 -0500

    bug fix

--- server/src/main/java/Pucknotes/Server/Report/Report.java
+++ server/src/main/java/Pucknotes/Server/Report/Report.java
@@ -5,6 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 
+import org.bson.types.ObjectId;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
