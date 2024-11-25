9a4f2c11c6825a08d57ce416496d206795ddc22d
mvhutz <pannus@maximhutz.me>
2024-11-25 17:04:15 -0500

    feat: comments for files

--- server/src/main/java/Pucknotes/Server/File/File.java
+++ server/src/main/java/Pucknotes/Server/File/File.java
@@ -5,11 +5,43 @@ import org.springframework.http.MediaType;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * The File class represents a file with its associated attributes such 
+ * as the filename, file type, file size, and the actual file content 
+ * in byte array format. This class makes use of Lombok annotations 
+ * to generate getters and setters for its fields automatically.
+ */
 @Getter
 @Setter
 public class File {
+
+    /**
+     * The name of the file. This field should hold the original filename
+     * as it was provided by the user or the system. It is essential for
+     * file identification and retrieval.
+     */
     private String filename;
+
+    /**
+     * The media type of the file, which indicates the format of the 
+     * file content (e.g., text/plain, image/jpeg). This information 
+     * can be useful for clients that need to know how to process or 
+     * display the file.
+     */
     private MediaType fileType;
+
+    /**
+     * The size of the file in bytes. This field helps in determining 
+     * if the file is too large or too small for specific operations 
+     * or requirements. Size validation can prevent storage issues or 
+     * performance degradation.
+     */
     private long fileSize;
+
+    /**
+     * The content of the file represented as a byte array. This is the 
+     * actual data that is stored in the file. Clients will typically 
+     * need to read or write this data to perform file operations.
+     */
     private byte[] file;
 }
--- server/src/main/java/Pucknotes/Server/File/FileService.java
+++ server/src/main/java/Pucknotes/Server/File/FileService.java
@@ -3,7 +3,6 @@ package Pucknotes.Server.File;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.mongodb.client.gridfs.model.GridFSFile;
-
 import org.apache.commons.io.IOUtils;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,18 +16,34 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
+/**
+ * FileService is a service class that manages file operations,
+ * including uploading, downloading, and deleting files stored in MongoDB GridFS.
+ */
 @Service
 public class FileService {
+    
     @Autowired
     private GridFsTemplate template;
     
     @Autowired
     private GridFsOperations operations;
 
+    /**
+     * Adds a file to the MongoDB GridFS.
+     *
+     * @param upload the MultipartFile object containing the file to be uploaded
+     * @return the ObjectId of the stored file
+     * @throws IOException if an error occurs while reading the file input stream or storing the file
+     * 
+     * This method creates a metadata object that includes the file size
+     * and stores the file using the GridFsTemplate. The object's ObjectId is returned.
+     */
     public ObjectId addFile(MultipartFile upload) throws IOException {
         DBObject metadata = new BasicDBObject();
         metadata.put("fileSize", upload.getSize());
 
+        // The input stream is obtained from the MultipartFile and stored in GridFS.
         return template.store(
             upload.getInputStream(),
             upload.getOriginalFilename(),
@@ -37,24 +52,45 @@ public class FileService {
         );
     }
 
+    /**
+     * Downloads a file from the MongoDB GridFS based on the provided ID.
+     *
+     * @param id the ID of the file to be downloaded
+     * @return a File object containing the downloaded file information
+     * @throws IOException if an error occurs while reading the file input stream
+     * 
+     * This method retrieves the file from GridFS using its ID, populating a 
+     * File object that includes the filename, content type, file size, and file bytes.
+     */
     public File downloadFile(String id) throws IOException {
         Query query = new Query(Criteria.where("_id").is(id));
         GridFSFile file = template.findOne(query);
         File result = new File();
 
+        // Checking if the file was found in GridFS and has metadata.
         if (file != null && file.getMetadata() != null) {
             result.setFilename(file.getFilename());
             result.setFileType(MediaType.parseMediaType(file.getMetadata().get("_contentType").toString()));
             result.setFileSize(Long.parseLong(file.getMetadata().get("fileSize").toString()));
 
+            // Converting the input stream to byte array for easy access.
             result.setFile(IOUtils.toByteArray(operations.getResource(file).getInputStream()));
         }
 
         return result;
     }
 
+    /**
+     * Deletes a file from the MongoDB GridFS based on the provided ID.
+     *
+     * @param id the ID of the file to be deleted
+     * 
+     * This method creates a query based on the file ID and deletes the file 
+     * from GridFS. It does not provide feedback if the file was not found.
+     */
     public void deleteFile(String id) {
         Query query = new Query(Criteria.where("_id").is(id));
         template.delete(query);
+        // Note: Additional error handling could be implemented here.
     }
 }
